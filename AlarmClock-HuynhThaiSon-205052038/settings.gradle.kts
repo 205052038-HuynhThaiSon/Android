9500daee005d5b369e0f6a434b4a7b7a026f8e3e
205052038-HuynhThaiSon <sonht220@uef.edu.vn>
2023-10-24 16:29:20 +0700

    Finish Project

---
+++ AlarmClock-HuynhThaiSon-205052038/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AlarmClock-HuynhThaiSon-205052038"
+include(":app")
